3b02ede17b1618011e82a0a663564ed5dda92b13
Andreslml1248 <nestor.nogales.gallo@alumnos.upm.es>
2026-03-03 11:55:14 +0100

    0.0025 y 0.00025

--- config/hardware.py
+++ config/hardware.py
@@ -142,7 +142,7 @@ OFFSET_2PT: float = 0.0
 # ============================
 # PI CONTROL (un solo PI para manual y auto)
 # ============================
-KP_DEFAULT: float = 0.0045
+KP_DEFAULT: float = 0.0025
 KI_DEFAULT: float = 0.00045
 
 DT_PI: float = 1.0 / 90.0
