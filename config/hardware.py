e8c2d84edb2f2b9af169a936b60c2f5b85a1a876
Andreslml1248 <nestor.nogales.gallo@alumnos.upm.es>
2026-03-03 11:18:49 +0100

    0.0045 y 0.00097 y 2Kpa de zona muerta

--- config/hardware.py
+++ config/hardware.py
@@ -149,7 +149,7 @@ DT_PI: float = 1.0 / 90.0
 U_MIN: float = 0.0
 U_MAX: float = 1.0
 
-DEADBAND_KPA: float = 1
+DEADBAND_KPA: float = 2
 U_FF: float = 0.38
 P_FILT_ALPHA: float = 1.0  # 1.0 = sin filtro
 
