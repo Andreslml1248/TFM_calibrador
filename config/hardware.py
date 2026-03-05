e85c9d226a49c118cf471af5c4b4a33d74f4536c
Andreslml1248 <nestor.nogales.gallo@alumnos.upm.es>
2026-03-05 17:41:45 +0100

    reduccion de variables

--- config/hardware.py
+++ config/hardware.py
@@ -142,16 +142,16 @@ OFFSET_2PT: float = 0.0
 # ============================
 # PI CONTROL (un solo PI para manual y auto)
 # ============================
-KP_DEFAULT: float = 0.0018
-KI_DEFAULT: float = 0.00030
+KP_DEFAULT: float = 0.0035
+KI_DEFAULT: float = 0.00006
 
 DT_PI: float = 0.08
 U_MIN: float = 0.0
 U_MAX: float = 1.0
 
-DEADBAND_KPA: float = 0.5
+DEADBAND_KPA: float = 1.0
 U_FF: float = 0.25
-P_FILT_ALPHA: float = 0.40 # 1.0 = sin filtro
+P_FILT_ALPHA: float = 1.0 # 1.0 = sin filtro
 
 # ============================
 # FFT / RUIDO
