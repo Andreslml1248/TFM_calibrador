a9f5b6f760dde84e65f5ab16961aa8fa6c8a5716
Andreslml1248 <nestor.nogales.gallo@alumnos.upm.es>
2026-03-08 12:59:33 +0100

    0.0030 0.00018 deadband 0.1 uff0.30

--- config/hardware.py
+++ config/hardware.py
@@ -158,7 +158,7 @@ DT_PI: float = 0.1
 U_MIN: float = 0.0
 U_MAX: float = 1.0
 
-DEADBAND_KPA: float = 1
+DEADBAND_KPA: float = 0.1
 U_FF: float = 0.30
 P_FILT_ALPHA: float = 1.0 # 1.0 = sin filtro
 
