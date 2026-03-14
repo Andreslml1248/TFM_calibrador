81cee671069eed419afb189f8b23c9afb0f91ad7
Andreslml1248 <nestor.nogales.gallo@alumnos.upm.es>
2026-03-14 19:46:38 +0100

    primera formula, 8 version

--- config/hardware.py
+++ config/hardware.py
@@ -157,8 +157,8 @@ KI_LOW: float = 0.0014258
 KP_MID: float = 0.002648828
 KI_MID: float = 0.00029108
 
-KP_HIGH: float = 0.01071765
-KI_HIGH: float = 0.0012609
+KP_HIGH: float = 0.0127776
+KI_HIGH: float = 0.0015972
 
 DT_PI: float = 0.1
 U_MIN: float = 0.0
