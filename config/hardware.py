6ea993cb896d28d84e5a0997181490a6ee2ff799
Andreslml1248 <nestor.nogales.gallo@alumnos.upm.es>
2026-03-15 12:59:25 +0100

    primera formula, 14 version

--- config/hardware.py
+++ config/hardware.py
@@ -151,8 +151,8 @@ OFFSET_2PT: float = 0.0
 # ============================
 # PI CONTROL (un solo PI para manual y auto)
 # ============================
-KP_LOW: float = 0.01383026
-KI_LOW: float = 0.0014258
+KP_LOW: float = 0.02429055
+KI_LOW: float = 0.0025569
 
 KP_MID: float = 0.002648828
 KI_MID: float = 0.00029108
