bb1a4e8a36b9aec0768149f3afaf432ea885d81b
Andreslml1248 <nestor.nogales.gallo@alumnos.upm.es>
2026-03-18 13:06:52 +0100

    Revert "prueba 6 LOW"
    This reverts commit a5539b456c946654b0b3e1500643bb062782eab5.

--- config/hardware.py
+++ config/hardware.py
@@ -151,7 +151,7 @@ OFFSET_2PT: float = 0.0
 # ============================
 # PI CONTROL (un solo PI para manual y auto)
 # ============================
-KP_LOW: float = 0.0020
+KP_LOW: float = 0.002308246
 KI_LOW: float = 0.0007
 
 KP_MID: float = 0.0030
