3ae678c8f8adaa50a34766c3c819a3177fb99edc
Andreslml1248 <nestor.nogales.gallo@alumnos.upm.es>
2026-03-18 13:01:48 +0100

    prueba 6 LOW

--- config/hardware.py
+++ config/hardware.py
@@ -152,7 +152,7 @@ OFFSET_2PT: float = 0.0
 # PI CONTROL (un solo PI para manual y auto)
 # ============================
 KP_LOW: float = 0.002308246
-KI_LOW: float = 0.0006
+KI_LOW: float = 0.0007
 
 KP_MID: float = 0.0030
 KI_MID: float = 0.00029108
