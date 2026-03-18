8649aae21b41530b194d089f3471f443ebe291f7
Andreslml1248 <nestor.nogales.gallo@alumnos.upm.es>
2026-03-18 11:37:30 +0100

    prueba 1 MEDIUM

--- config/hardware.py
+++ config/hardware.py
@@ -154,7 +154,7 @@ OFFSET_2PT: float = 0.0
 KP_LOW: float = 0.01383026
 KI_LOW: float = 0.0025569
 
-KP_MID: float = 0.002648828
+KP_MID: float = 0.0030
 KI_MID: float = 0.00029108
 
 KP_HIGH: float = 0.01865592
